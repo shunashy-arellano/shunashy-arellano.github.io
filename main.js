db609f712d61f32ed7d58310443f4f1a74c4382e
Shu Nashy Arellano Aguillón <85764530+shunashy-arellano@users.noreply.github.com>
2022-12-13 19:41:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let app = document.getElementById('typewriter');
+
+
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+
+
+
+typewriter
+  .pauseFor(2500)
+  .typeString('Desarrollo sitios web y soy estudiante en Tecnolochicaspro.')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
